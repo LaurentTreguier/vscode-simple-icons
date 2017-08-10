9fadbd278bbca54c0aa8875e60e720a719d321a7
Laurent Tréguier <laurent@treguier.org>
2017-08-10 21:04:04 +0100

    Beautify files before generating everything

--- generator.sh
+++ generator.sh
@@ -16,6 +16,25 @@ function get_color() {
     grep -Eo '(rect|polygon)[^#]+fill="#[0-9a-fA-F]{6}"' $1 | grep -Eo '#.{6}'
 }
 
+function validate_sums() {
+    if [[ -f $1 ]] && [[ -f $2 ]]
+    then
+        sum="$(tail -1 $2 | grep -Eo '\w+' | head -1)"
+        if [[ "$(sha256sum $1 | grep -Eo '\w+' | head -1)" = "$sum" ]]
+        then
+            return 0
+        else
+            return 1
+        fi
+    else
+        return 1
+    fi
+}
+
+function comment_sum() {
+    echo "<!-- $(sha256sum $1) -->"
+}
+
 mkdir -p {$simple_gen_dir,$simple_icons_dir,$mini_gen_dir,$mini_icons_dir}
 
 for file in $(ls $simple_icons_dir)
@@ -29,8 +48,9 @@ done
 
 for file in $(ls $simple_gen_dir)
 do
-    if [[ $file != *.folder.expanded.svg ]] || [[ ! -f $simple_source_dir/${file/.expanded/} ]]
+    if [[ $file != *.folder.expanded.svg ]] || [[ ! -f $simple_source_dir/${file/.expanded/} ]] || [[ -f $simple_source_dir/$file ]]
     then
+        echo "Cleaning up unused simple icon $file"
         rm -f $simple_gen_dir/$file
     fi
 done
@@ -44,18 +64,26 @@ do
     fi
 done
 
+for theme_source_dir in $mini_source_dir $simple_source_dir
+do
+    echo "Beautifying icons from $theme_source_dir"
+    ./node_modules/.bin/svgo --config=.svgo.yml --multipass -f $theme_source_dir > /dev/null
+done
+
 for folder in $(ls $simple_source_dir/*.folder.svg)
 do
     expanded_folder=${folder/.svg/.expanded.svg}
     gen_folder=$simple_gen_dir/$(basename $expanded_folder)
+    validate_sums $folder $gen_folder
 
-    if [[ ! -f $expanded_folder ]] && [[ $folder -nt $gen_folder ]]
+    if [[ $? -ne 0 ]] && [[ ! -f $expanded_folder ]]
     then
         echo "Generating simple $(basename $expanded_folder)"
         old_color=$(get_color $simple_source_dir/folder.expanded.svg)
         new_color=$(get_color $folder)
         cp $simple_source_dir/folder.expanded.svg $gen_folder
         sed -ri "s/$old_color/$new_color/g" $gen_folder
+        comment_sum $folder >> $gen_folder
     fi
 done
 
@@ -74,20 +102,24 @@ do
         rm -f $mini_gen_dir/$file
     else
         file_dir=$mini_gen_dir
+        validate_sums $simple_dir/$file $mini_gen_dir/$file
 
-        if [[ $simple_dir/$file -nt $mini_gen_dir/$file ]]
+        if [[ $? -ne 0 ]]
         then
             echo "Generating minimalistic $file"
             node generator.js gen < $simple_dir/$file > $mini_gen_dir/$file
+            comment_sum $simple_dir/$file >> $mini_gen_dir/$file
         fi
     fi
 
     light_file=${file/.svg/.light.svg}
+    validate_sums $file_dir/$file $mini_gen_dir/$light_file
 
-    if [[ ! -f $mini_gen_dir/$light_file ]] || [[ $file_dir/$file -nt $mini_gen_dir/$light_file ]]
+    if [[ $? -ne 0 ]]
     then
         echo "Generating minimalistic $light_file"
         node generator.js light < $file_dir/$file > $mini_gen_dir/$light_file
+        comment_sum $file_dir/$file >> $mini_gen_dir/$light_file
     fi
 done
 
