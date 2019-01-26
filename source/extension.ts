171e263f6cb1ee1388c74173d8ebbee4e4e5366f
Laurent Tréguier <laurent@treguier.org>
2019-01-26 11:49:14 +0100

    Use async functions

--- source/extension.ts
+++ source/extension.ts
@@ -2,45 +2,43 @@ import * as fs from 'fs';
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-    vscode.workspace.onDidChangeConfiguration(() => toggleArrows(context));
+    context.subscriptions.push(vscode.workspace.onDidChangeConfiguration(() => toggleArrows(context)));
     toggleArrows(context);
 }
 
 export function deactivate() {
 }
 
-function toggleArrows(context: vscode.ExtensionContext) {
-    const jsonFiles = ['simple', 'minimalistic']
-        .map(name => context.asAbsolutePath(name + '-icons.json'));
+async function toggleArrows(context: vscode.ExtensionContext) {
+    const jsonFiles = ['simple', 'minimalistic'].map(name => context.asAbsolutePath(name + '-icons.json'));
+    let somethingChanged = false;
 
-    Promise.all(jsonFiles.map(file =>
-        new Promise(resolve => fs.readFile(file, (err, data) => resolve(data)))
-            .then(data => JSON.parse(data.toString()))
-            .then(json => {
-                let conf = !!vscode.workspace.getConfiguration('simpleIcons').get('hideArrows', false);
+    for (const file of jsonFiles) {
+        await changeFile(file, content => {
+            let json = JSON.parse(content);
+            const conf = vscode.workspace.getConfiguration('simpleIcons').get('hideArrows', false);
 
-                if (json.hidesExplorerArrows === conf) {
-                    return null;
-                }
-
-                json.hidesExplorerArrows = conf;
-                return JSON.stringify(json, null, 4);
-            })
-            .then(jsonString => {
-                if (jsonString) {
-                    new Promise(resolve => fs.writeFile(file, jsonString, resolve))
-                }
-
-                return !!jsonString;
-            })))
-        .then((results) => {
-            if (results.indexOf(true) !== -1) {
-                return vscode.window.showInformationMessage('The window must be reloaded for changes to take effet', 'Reload');
+            if (json.hidesExplorerArrows === conf) {
+                return null;
             }
-        })
-        .then(choice => {
-            if (choice) {
-                vscode.commands.executeCommand('workbench.action.reloadWindow');
-            }
+
+            somethingChanged = true;
+            json.hidesExplorerArrows = conf;
+            return JSON.stringify(json, null, 4);
         });
+    }
+
+    if (somethingChanged) {
+        vscode.window.showInformationMessage('The window must be reloaded for changes to take effet', 'Reload')
+            .then(choice => choice ? vscode.commands.executeCommand('workbench.action.reloadWindow') : null);
+    }
+}
+
+async function changeFile(name: string, dg: (content: string) => string) {
+    const content = await new Promise<string>(resolve => fs.readFile(name, (err, data) => resolve(data.toString())));
+    const newContent = dg(content);
+
+    if (newContent) {
+        return await new Promise<void>(resolve => fs.writeFile(name, newContent, err => resolve()));
+    }
 }
